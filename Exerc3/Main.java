a91e4a32439325c6e42483e509eb145065863286
Kenedy Madueke <kenedy.max17@gmail.com>
2025-10-23 20:27:45 -0300

    Exerc3 concluido

--- Exerc3/Main.java
+++ Exerc3/Main.java
@@ -1,9 +1,17 @@
 package Exerc3;
 
+import java.util.Scanner;
+
 public class Main {
     
     public static void main(String[] args) {
-        
+        var scanner = new Scanner(System.in);
+        System.out.println("Informe a base do retangulo:");
+        var base = scanner.nextInt();
+        System.out.println("Informe a altura do retangulo");
+        var altura = scanner.nextInt();
+        var area = base * altura;
+        System.out.printf("A area do retangulo de base %s e altura %s e %s \n", base, altura, area);
     }
 }
 /*
