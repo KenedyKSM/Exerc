d4f9e0d8514d34b6bbb97e77ea9f421ae5c48c8c
Kenedy Madueke <kenedy.max17@gmail.com>
2025-10-23 19:57:08 -0300

    Exerc2 concluido

--- Exerc2/Main.java
+++ Exerc2/Main.java
@@ -1,9 +1,18 @@
 package Exerc2;
 
+import java.util.Scanner;
+
 public class Main {
+
+    public static void main(String[] args) {
+        var scanner = new Scanner(System.in);
+        System.out.println("Informe o tamanho do lado do quadrado:");
+        var side = scanner.nextInt();
+        var area = side * side;
+        System.out.printf("A area do quadrado de lado %s e %s \n", side, area);
+    }
     
 }
-
 
 /*
  Escreva um codigo que receba o tamanho do lado do quadrado, calcule sua area e exiba na tela
