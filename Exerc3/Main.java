840e7a061a3fa9e5434629ae131931a6448ad673
Kenedy Madueke <kenedy.max17@gmail.com>
2025-10-23 20:20:40 -0300

    add comentario ao Exerc3

---
+++ Exerc3/Main.java
@@ -0,0 +1,13 @@
+package Exerc3;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        
+    }
+}
+/*
+ Escreva um codigo que receba a base a altura de um retangulo, calcule sua area e exiba na tela
+
+ - formula: area = base X altura
+ */
